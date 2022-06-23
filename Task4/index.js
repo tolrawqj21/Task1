44c6711e9dbe2665f134b8208189e4c34ad0ca29
Irina <umniakova.i@gmail.com>
2022-06-23 18:46:15 +0300

    completed task 5

--- Task4/index.js
+++ Task4/index.js
@@ -1,51 +1,54 @@
 //1
-let arr =['455', 87.15, true, undefined, 7, null, 'false',[], {}];
-let arr_2=[];
-let num=3;
-arr.forEach(element => console.log( typeof element));
-
-arr.forEach(function(element) {
-    if(typeof element==='number') arr_2.push(element);
-});
-console.log(arr_2);
-let arr_2_2= arr_2.map(x=>x+num);
-console.log(arr_2_2);
+// const dataTypes = ['455', 87.15, true, undefined, 7, null, 'false', [], {}];
+// let numbers = [];
+// const termNum = 3;
+// dataTypes.forEach(element => console.log(typeof element));
+//
+// dataTypes.forEach(function (element) {
+//     if (typeof element === 'number') numbers.push(element);
+// });
+// console.log(numbers);
+//
+// // const res = numbers.map(x => x + termNum);
+//
+//  const res = numbers.reduce
+// console.log(res);
 
 //2
-let num= prompt('Введите целое число','');
-let arr=[];
-for(let i=0; i<num; i++){
-    arr.push (Math.floor(Math.random()*10))
-}
-console.log(arr);
-
-let arr_2= [...arr];
-console.log(arr_2);
-for(let i=2; i<arr_2.length; i+=3){
-  arr_2[i]*=3;
-}
-console.log(arr_2);
+// let num= prompt('Введите целое число','');
+// let arr=[];
+// for(let i=0; i<num; i++){
+//     arr.push (Math.floor(Math.random()*10))
+// }
+// console.log(arr);
+//
+// let arr_2= [...arr];
+// console.log(arr_2);
+// for(let i=2; i<arr_2.length; i+=3){
+//   arr_2[i]*=3;
+// }
+// console.log(arr_2);
 
 //3
-let student1= {
-    firstName: 'Ira',
-    lastName:'Umniakova',
-    mark: 10
-};
-console.log(student1);
-let student2 ={...student1, mark: 12};
-console.log(student2);
-
-let student3= Object.assign({}, student1, {mark: 100});
-console.log(student3);
-
-let marks= (student1.mark+student2.mark+student3.mark)/3;
-console.log(marks);
-
-console.log(student1.mark);
-console.log(student2.mark);
-console.log(student3.mark);
-
-for(let key in student1){
-console.log(key,student1[key]);
-}
+// let student1= {
+//     firstName: 'Ira',
+//     lastName:'Umniakova',
+//     mark: 10
+// };
+// console.log(student1);
+// let student2 ={...student1, mark: 12};
+// console.log(student2);
+//
+// let student3= Object.assign({}, student1, {mark: 100});
+// console.log(student3);
+//
+// let marks= (student1.mark+student2.mark+student3.mark)/3;
+// console.log(marks);
+//
+// console.log(student1.mark);
+// console.log(student2.mark);
+// console.log(student3.mark);
+//
+// for(let key in student1){
+// console.log(key,student1[key]);
+// }
---
+++ Task5/index.js
@@ -0,0 +1,52 @@
+//1
+const randomArr = [...Array(15)].map(() =>
+    Math.floor(Math.random() * 20)
+);
+//2
+let results = randomArr.filter(function (num) {
+    return num % 3 === 0;
+});
+
+//3
+let bool= false;
+for (let i = 0; i < randomArr.length; i++) {
+    if (randomArr[i] > 7) {
+        bool=true;
+        console.log(randomArr[i]);
+        break;
+    }
+}
+if (!bool) {
+    console.log('Все числа меньше 7');
+}
+
+//4
+let newArr = randomArr.map((x) => x * 2);
+
+//5
+let sum = 0;
+for (let i = 0; i < randomArr.length; i++) {
+    if (randomArr[i] % 2 === 0) {
+        sum += randomArr[i];
+    }
+}
+
+console.log(randomArr);
+console.log(results);
+console.log(newArr);
+console.log(sum);
+
+//6
+console.log(randomArr.shift());
+//7
+console.log(randomArr.pop());
+
+//со звездочкой
+
+// const arrayOne = ['dog', 'cat', 'bird', 'rabbit', 'horse'];
+// const arrayTwo = ['apple', 'car', 'cat', 'ball', 'horse'];
+// let arr= [];
+//
+// for (let i=0; i < arr.length; i++){
+//
+// }
