57ebe8efe4e26a34d27c01958eed3d6daf0f3b89
Irina <umniakova.i@gmail.com>
2022-07-17 16:04:14 +0300

    almost did 10 task

--- Task10/index.js
+++ Task10/index.js
@@ -13,26 +13,40 @@
 // coffeeMachine.start.call(teaPlease);
 
 //2
-function concatStr(str1, sep, nameee){
-return str1+ sep+ nameee;
-}
-function hello(nameee){
-concatStr().bind()
-}
-let checkConcat = concatStr('Hello', ' ', 'Matt'); // 'Hello Matt'
-let finalResult = hello('John') // 'Hello Matt'
-
-console.log(checkConcat);
-console.log(finalResult);
+// function concatStr(str1, sep, name) {
+//     return str1 + sep + name;
+// }
+// let hello =concatStr.bind(null,'Hello',' ');
+// let checkConcat = concatStr('Hello', ' ', 'Matt'); // 'Hello Matt'
+// let finalResult = hello('John') // 'Hello Matt'
+// console.log(checkConcat);
+// console.log(finalResult);
 
 
 //3
-// function showNumber(min, max,time){
-// setTimeout(){
+// function showNumber(min, max,time) {
 //
-//     }
+// setTimeout(()=>showNumber(),500)
+//        if(min<max){
+//            setTimeout()
+//        }
 // }
-// showNumber(5,10,1000);
+// showNumber(5, 10,500);
+
+
+function showText(num) {
+    setTimeout(()=>{
+        showText(num);
+    }, 2000)
+    console.log("Hello");
+}
+
+showText(5);
+
+
+// setTimeout(() => console.log('Привет'), 1000);
+// setInterval(() => console.log('Привет'), 5000);
+
 
 //4
 // function addBase(base) {
